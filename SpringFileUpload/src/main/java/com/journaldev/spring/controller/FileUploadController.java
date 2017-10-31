c8b78b6f929ffa035fab1ca398b86a42bbc4f601
subhashish-ghosh <ghosh.subhashish@gmail.com>
2017-10-30 21:33:08 -0500

    no message

---
+++ SpringFileUpload/src/main/java/com/journaldev/spring/controller/FileUploadController.java
@@ -0,0 +1,156 @@
+package com.journaldev.spring.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+/**
+ * Handles requests for the application file upload requests
+ */
+@Controller
+public class FileUploadController {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(FileUploadController.class);
+	
+	private static final String SUFFIX = "/";
+
+	/**
+	 * Upload single file using Spring Controller
+	 */
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+	public @ResponseBody
+	String uploadFileHandler(@RequestParam("name") String name,
+			@RequestParam("file") MultipartFile file) {
+
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+				
+				Path path = Paths.get("C:\\Subhashish\\Learning\\Repo\\"+file.getOriginalFilename());
+				
+				//FileOutputStream out = new FileOutputStream("the-file-name");
+				
+			    Files.write(path, bytes);
+			    
+			    File convFile = new File( file.getOriginalFilename());
+			    file.transferTo(convFile);
+
+				/*// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath()
+						+ File.separator + name);
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+				logger.info("Server File Location="
+						+ serverFile.getAbsolutePath());*/
+			    
+			    
+			    AWSCredentials credentials = new BasicAWSCredentials("AKIAIO62HKXVYZYS2ZAA",
+						"Vk4MGyg8ks2dk+ep9e8cDWudpAobcXF5cN6BUi+V");
+
+				// create a client connection based on credentials
+				AmazonS3 s3client = new AmazonS3Client(credentials);
+
+				// create bucket - name must be unique for all S3 users
+				String bucketName = "subhbucketa";
+				s3client.createBucket(bucketName);
+
+				// list buckets
+				/*for (Bucket bucket : s3client.listBuckets()) {
+					System.out.println(" - " + bucket.getName());
+				}*/
+
+				// create folder into bucket
+				String folderName = "testfolder";
+				
+				String fileName = folderName + SUFFIX + file.getOriginalFilename();
+				
+				s3client.putObject(new PutObjectRequest(bucketName, fileName, convFile)
+						 .withCannedAcl(CannedAccessControlList.PublicRead));
+				
+			    
+			    
+			    
+
+				return "You successfully uploaded file=" + name;
+			} catch (Exception e) {
+				return "You failed to upload " + name + " => " + e.getMessage();
+			}
+		} else {
+			return "You failed to upload " + name
+					+ " because the file was empty.";
+		}
+	}
+
+	/**
+	 * Upload multiple file using Spring Controller
+	 */
+	@RequestMapping(value = "/uploadMultipleFile", method = RequestMethod.POST)
+	public @ResponseBody
+	String uploadMultipleFileHandler(@RequestParam("name") String[] names,
+			@RequestParam("file") MultipartFile[] files) {
+
+		if (files.length != names.length)
+			return "Mandatory information missing";
+
+		String message = "";
+		for (int i = 0; i < files.length; i++) {
+			MultipartFile file = files[i];
+			String name = names[i];
+			try {
+				byte[] bytes = file.getBytes();
+
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath()
+						+ File.separator + name);
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+				logger.info("Server File Location="
+						+ serverFile.getAbsolutePath());
+
+				message = message + "You successfully uploaded file=" + name
+						+ "<br />";
+			} catch (Exception e) {
+				return "You failed to upload " + name + " => " + e.getMessage();
+			}
+		}
+		return message;
+	}
+}
