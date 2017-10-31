2c97356b535e2e6a12b7dd6fb0405ee438298ded
subhashish-ghosh <ghosh.subhashish@gmail.com>
2017-10-30 21:11:57 -0500

    no message

---
+++ file-api/greetings1.js
@@ -0,0 +1,44 @@
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-east-1:efd4522f-b2e0-4044-8465-91a963b0bda8',
+});
+
+var lambda = new AWS.Lambda();
+
+function returnGreetings() {
+  document.getElementById('submitButton').disabled = true;
+  var name = document.getElementById('srcText');
+  var input;
+  if (name.value == null || name.value == '') {
+    input = {};
+  } else {
+    input = {
+      name: name.value
+    };
+  }
+  lambda.invoke({
+    FunctionName: 'subhFunction',
+    Payload: JSON.stringify(name.value)
+  }, function(err, data) {
+    var result = document.getElementById('result');
+    if (err) {
+      console.log(err, err.stack);
+      result.innerHTML =
+        '<div class="alert alert-danger">' + err + '</div>';
+    } else {
+	  alert(JSON.stringify(data.Payload));
+      var output = JSON.parse(data.Payload);
+	  
+      result.innerHTML =
+        '<div class="alert alert-success">' + output + '</div>';
+    }
+    document.getElementById('submitButton').disabled = false;
+  });
+}
+
+var form = document.getElementById('greetingsForm');
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  returnGreetings();
+});
