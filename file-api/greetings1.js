d2b087601444b2d7e3819121be47ab02f57e47c6
subhashish-ghosh <ghosh.subhashish@gmail.com>
2017-11-01 22:04:28 -0500

    no message

--- file-api/greetings1.js
+++ file-api/greetings1.js
@@ -1,7 +1,7 @@
 // Initialize the Amazon Cognito credentials provider
 AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: 'us-east-1:efd4522f-b2e0-4044-8465-91a963b0bda8',
+  IdentityPoolId: 'POOL_ID',
 });
 
 var lambda = new AWS.Lambda();
